a06eb4f4308155103463eedf74efb9db14dec82d
Bruno Bezerra <akabezerra@icloud.com>
2022-06-28 19:36:33 -0300

    ajustes

--- MovieRoll/View/Roleta/RoletaViewController.swift
+++ MovieRoll/View/Roleta/RoletaViewController.swift
@@ -35,15 +35,6 @@ class RoletaViewController: UIViewController {
         inicializaCollectionView()
         inicializaTextField()
         inicializaPickerView()
-        
-        for family: String in UIFont.familyNames
-        {
-            print(family)
-            for names: String in UIFont.fontNames(forFamilyName: family)
-            {
-                print("== \(names)")
-            }
-        }
     }
     
     
